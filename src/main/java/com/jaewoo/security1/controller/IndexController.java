c7a59cd558526c80734f6722c66cd09cef1d7b4a
Jung Jaewoo <jaewo7o@naver.com>
2022-05-01 18:17:54 +0900

    basic security setting

--- src/main/java/com/jaewoo/security1/controller/IndexController.java
+++ src/main/java/com/jaewoo/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.jaewoo.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,5 +12,35 @@ public class IndexController {
         System.out.println("test!!!123");
 
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/join-proc")
+    public @ResponseBody String joinProc() {
+        return "joinProc";
     }
 }
